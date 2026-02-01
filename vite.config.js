8eb77dd03f6d405c9d54fce813e6b6f8d2bf77e7
fareehaasghar487-cloud <fareehaasghar487@gmail.com>
2026-02-01 20:15:35 +0500

    Change vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  base: "/", // ✅ ADD THIS
+  plugins: [react(), tailwindcss()],
 })
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(),tailwindcss(),],
-// })
